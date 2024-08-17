ee449b4b1b42950b8bfc857ff788d88df9ec3396
ttangzzi <cvbg0802@gmail.com>
2024-08-17 13:46:51 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -5,31 +5,53 @@ import './App.css';
 import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 import bg from './img/bg.png';
 import data from './data'
+import Detail from './routes/component'
+import { Routes, Route, Link, useNavigate, Outlet} from "react-router-dom"
 
 function App() {
   let [shoes] = useState(data)
+  let navigate = useNavigate();
   return (
     <div className="App">
+
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">TTANG</Navbar.Brand>
+          <Navbar.Brand href="#">TTANG</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link href="/detail">Features</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg'></div>
-      <Row>
-        {
-          data.map(function(a, i) {
-            return (
-              <Product image={`https://codingapple1.github.io/shop/shoes${i+1}.jpg`} title={shoes[i].title} content={shoes[i].content}/>
-            )
-          })
-        }
-      </Row>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+          <div className='main-bg'></div>
+          <Row>
+            {
+              data.map(function(a, i) {
+              return (
+                <Product 
+                image={`https://codingapple1.github.io/shop/shoes${i+1}.jpg`} 
+                title={shoes[i].title} 
+                content={shoes[i].content}/>
+              )
+              })
+            }
+          </Row>
+          </>
+        }/>
+        <Route path="/detail" element={<Detail/>}/>
+        <Route path="*" element={<div>404 페이지</div>}/>
+        <Route path="/about" element={<About/>}>
+          <Route path="member" element={<div>멤버</div>}/>
+          <Route path="location" element={<div>지역</div>}/>
+        </Route>
+      </Routes>
+
+
     </div>
   );
 }
@@ -43,6 +65,15 @@ function Product(props) {
       <h4>{props.title}</h4>
       <p>{props.content}</p>
     </Col>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보</h4>
+      <Outlet></Outlet>
+    </div>
   )
 }
 
---
+++ src/routes/component.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import { Container, Row, Col } from 'react-bootstrap';
+function Detail() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </Col>
+        <Col>
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </Col>
+      </Row>
+    </Container> 
+  )
+}
+
+export default Detail;
