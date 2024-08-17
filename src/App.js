ae71e1a2fb03b009d69c234632e5044ae3315637
ttangzzi <cvbg0802@gmail.com>
2024-08-17 13:52:34 +0900

    route 숙제

--- src/App.js
+++ src/App.js
@@ -49,8 +49,12 @@ function App() {
           <Route path="member" element={<div>멤버</div>}/>
           <Route path="location" element={<div>지역</div>}/>
         </Route>
-      </Routes>
 
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+      </Routes>
 
     </div>
   );
@@ -72,6 +76,15 @@ function About() {
   return (
     <div>
       <h4>회사정보</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
       <Outlet></Outlet>
     </div>
   )
