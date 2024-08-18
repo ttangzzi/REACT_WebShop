a65da9ba51e66689b8bc78dd70e262de508b7e8d
ttangzzi <cvbg0802@gmail.com>
2024-08-18 21:59:05 +0900

    styled-components 사용

--- src/routes/component.js
+++ src/routes/component.js
@@ -1,6 +1,19 @@
 /* eslint-disable */
 import { Container, Row, Col } from 'react-bootstrap';
 import { useParams } from "react-router-dom";
+import styled from 'styled-components'
+
+let ColorBtn = styled.button`
+  background : ${ props => props.bg };
+  color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
+  padding : 10px;
+`
+let Box = styled.div`
+  background : grey;
+  padding : 20px;
+`
+
+
 function Detail(props) {
   let {id} = useParams();
   const shoe = props.shoes.find(function(item) { return item.id === Number(id)})
@@ -10,6 +23,8 @@ function Detail(props) {
   else {
     return (
       <Container>
+        <ColorBtn bg = "blue">버튼</ColorBtn>
+        <ColorBtn bg = "yellow">버튼</ColorBtn>
         <Row>
           <Col>
             <img src={`https://codingapple1.github.io/shop/shoes${shoe.id +1}.jpg`} width="100%" />
