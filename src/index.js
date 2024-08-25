afc895881f6453e5bbf5814e70add24b66aca87a
ttangzzi <cvbg0802@gmail.com>
2024-08-25 17:25:40 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './store.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store}> 
       <BrowserRouter>
         <App />
       </BrowserRouter>
